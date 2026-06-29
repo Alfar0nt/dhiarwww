d4107a778d31f6e396f28bb0c536f9c97e8cc330
Dhiar Harianto <141720724+Alfar0nt@users.noreply.github.com>
2026-06-29 20:56:45 +0700

    refac: resume link

--- links/script.js
+++ links/script.js
@@ -18,8 +18,8 @@ const links = [
     },
     {
         title: "Curiculum Vitae",
-        description: "My CV or personal Resume. (You need to request access first)",
-        url: "https://drive.google.com/file/d/1NYIONo-DlEVeUiP2UW6Sch7AeS0siAjG/view?usp=drive_link",
+        description: "My CV or personal Resume.",
+        url: "https://drive.google.com/file/d/1ZFVtF6Rs13ihJ0W-xy1Q6tcHSO65xzvH/view?usp=sharing",
         // Inline SVG — zero HTTP request
         svgIcon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`
     },
